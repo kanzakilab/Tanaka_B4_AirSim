a8e8eb750bc60ff37b150d7068a0438cefb24c2d
Jetjaguar-PSX <s195056@matsu.shimane-u.ac.jp>
2022-12-02 12:58:13 +0900

    1201-1202

---
+++ Unity/UnityDemo/Assets/Scripts/Test_r1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using AirSimUnity.DroneStructs;
+using UnityEngine;
+
+namespace AirSimUnity
+{
+    [RequireComponent(typeof(Drone))]
+    public class Test_r1 : MonoBehaviour
+    {
+        private Drone drone;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            drone = this.gameObject.GetComponent<Drone>();
+        }
+
+        private void LateUpdate()
+        {
+            if (drone.ControllerAllowed())
+            {
+                AirSimRCData rcData = drone.GetRCData();
+
+                rcData.is_valid = true;
+                rcData.roll = 0.65f; //左右
+                rcData.pitch = 0; //前後
+                rcData.throttle = 1.0f; //上下
+                rcData.yaw = 0; //回転
+                rcData.left_z = 0;
+                rcData.right_z = 0;
+
+                drone.SetRCData(rcData);
+            }
+        }
+    }
+}
