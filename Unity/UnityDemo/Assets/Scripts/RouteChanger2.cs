7e720660d1bbcc19b08c6f46dd7fb156afceb179
Jetjaguar-PSX <s195056@matsu.shimane-u.ac.jp>
2023-01-30 11:00:00 +0900

    経路変更関連

---
+++ Unity/UnityDemo/Assets/Scripts/RouteChanger2.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AirSimUnity.DroneStructs;
+using UnityEngine;
+namespace AirSimUnity
+{
+    [RequireComponent(typeof(Drone))]
+    public class RouteChanger2 : MonoBehaviour
+    {
+        private Drone drone;
+        [SerializeField] private GameObject OtherDrone;
+        private float inclination1;
+        private float inclination2;
+        private Vector3 pos1;
+        private Vector3 pos2;
+        private float dis;
+        [SerializeField] private int direction;
+        [SerializeField] private float power;
+        RouteChanger2 otherscript;
+        private int recieved = 0;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            drone = this.gameObject.GetComponent<Drone>();
+            otherscript = OtherDrone.GetComponent<RouteChanger2>();
+            inclination1 = 0.6f * direction;
+            inclination2 = 0.0f;
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            pos1 = this.gameObject.transform.position;
+            pos2 = OtherDrone.transform.position;
+            dis = Vector3.Distance(pos1, pos2);
+            if (dis <= 50.0f)
+            {
+                otherscript.connect();
+            }
+            if (recieved >= 240)
+            {
+                inclination1 = -0.5f * direction;
+                inclination2 = -0.5f * direction;
+                power = 1.0f;
+            }
+            Debug.Log(direction * recieved);
+        }
+
+        private void LateUpdate()
+        {
+            if (drone.ControllerAllowed())
+            {
+                AirSimRCData rcData = drone.GetRCData();
+
+                rcData.is_valid = true;
+                rcData.roll = inclination1; //左右
+                rcData.pitch = inclination2; //前後
+                rcData.throttle = power; //上下
+                rcData.yaw = 0; //回転
+                rcData.left_z = 0;
+                rcData.right_z = 0;
+
+                drone.SetRCData(rcData);
+            }
+        }
+
+        void connect()
+        {
+            recieved++;
+            otherscript.ack();
+        }
+
+        void ack()
+        {
+            recieved++;
+        }
+    }
+}
---
+++ Unity/UnityDemo/Assets/Scripts/SaveTraceData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+public class SaveTraceData : MonoBehaviour
+{
+    private StreamWriter sw;
+    private int framecount = 0;
+    private float posx = 0;
+    private float posy = 0;
+    private float posz = 0;
+    //private string[] s1;
+    //private string s2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sw = new StreamWriter(@"C:\Users\jetst\Downloads\SaveData1.csv", true, Encoding.GetEncoding("Shift_JIS"));
+        string[] s1 = { "Frame", "x", "y", "z" };
+        string s2 = string.Join(",", s1);
+        sw.WriteLine(s2);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        framecount++;
+        posx = this.gameObject.transform.position.x;
+        posy = this.gameObject.transform.position.y;
+        posz = this.gameObject.transform.position.z;
+        string[] s1 = { framecount.ToString(), posx.ToString(), posy.ToString(), posz.ToString() };
+        string s2 = string.Join(",", s1);
+        sw.WriteLine(s2);
+    }
+
+    void OnApplicationQuit()
+    {
+        sw.Close();
+    }
+}
